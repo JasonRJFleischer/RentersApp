be1cd21c01439f746ca88748cb26e82a48761293
Fred Astaire <lancebc@thegodardgroup.com>
2020-01-25 14:10:39 -0500

    minor tweaks to NavBar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,49 +1,52 @@
 import React from 'react';
+import logo from "../images/house.png"
 
 // stateless functional component
 
 const NavBar = props => {
     return (
-        <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href='/'>
-                <h3>Renters App{" "}</h3>
-            </a>
-            <button className="btn btn-sm allEvictions"
-                onClick={function () {
-                    window.location.replace("/");
-                }}
-            >
-                All Evictions{" "}
-            </button>
-            <button className="btn btn-sm landlordSearch"
-                onClick={function () {
-                    window.location.replace("/landlordSearch");
-                }}
-            >
-                Look up Landlord{" "}
-            </button>
-            <button className="btn btn-sm addressSearch"
-                onClick={function () {
-                    window.location.replace("/addressSearch");
-                }}
-            >
-                Look up Address{" "}
-            </button>
-            <button className="btn btn-sm streetSearch"
-                onClick={function () {
-                    window.location.replace("/streetSearch");
-                }}
-            >
-                Look up Street Name{" "}
-            </button>
-            <button className="btn btn-sm zipSearch"
-                onClick={function () {
-                    window.location.replace("/zipSearch");
-                }}
-            >
-                Look up Zip Code{" "}
-            </button>
-        </nav>);
+        <div className="container">
+            <nav className="navbar fixed-top navbar-light bg-light">
+                <a className="navbar-brand" href='/'>
+                    <h3><img src={logo} style={{ width: 50, height: 50 }} alt="logo" />  Renters App{" "}</h3>
+                </a>
+                <button className="btn btn-sm allEvictions"
+                    onClick={function () {
+                        window.location.replace("/");
+                    }}
+                >
+                    All Evictions{" "}
+                </button>
+                <button className="btn btn-sm landlordSearch"
+                    onClick={function () {
+                        window.location.replace("/landlordSearch");
+                    }}
+                >
+                    Look up Landlord{" "}
+                </button>
+                <button className="btn btn-sm addressSearch"
+                    onClick={function () {
+                        window.location.replace("/addressSearch");
+                    }}
+                >
+                    Look up Address{" "}
+                </button>
+                <button className="btn btn-sm streetSearch"
+                    onClick={function () {
+                        window.location.replace("/streetSearch");
+                    }}
+                >
+                    Look up Street Name{" "}
+                </button>
+                <button className="btn btn-sm zipSearch"
+                    onClick={function () {
+                        window.location.replace("/zipSearch");
+                    }}
+                >
+                    Look up Zip Code{" "}
+                </button>
+            </nav >
+        </div>)
 }
 
 export default NavBar;
