384b31db835d3ad7312f05f72312202813148bf0
Jason Fleischer <jason.r.j.fleischer@gmail.com>
2020-04-09 19:24:06 -0400

    Create custom api for evictions

--- django_app/landlord_finder/views.py
+++ django_app/landlord_finder/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from .models import Landlord, Address, Case
 from django.views.generic import ListView, DetailView, TemplateView
-from .models import Landlord, Address
-from .serializers import LandlordSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class LandlordListView(ListView):
@@ -12,14 +11,26 @@ class LandlordListView(ListView):
 class AddressDetailView(DetailView):
     model = Address
 
+class LandlordListCreate(APIView):
 
-class LandlordListCreate(ListCreateAPIView):
-    queryset = Landlord.objects.all()
-    serializer_class = LandlordSerializer
-
-    # landlord.case_set.all
-    # for address in landlord.case_set.all
-
+    def get(self, request, format=None):
+        data = []
+        for case in Case.objects.all():
+            data.append({
+                            "caseID": case.case_id,
+                            "address": {
+                                "addressLine1": case.address.address1,
+                                "addressLine2": case.address.address2,
+                                "addressLine3": case.address.address3,
+                                "zip": case.address.zip_code,
+                                "city": case.address.city,
+                                "state": case.address.state
+                                },
+                            "plaintiff": case.landlord.name,
+                            "filingDate": case.filing_date,
+                            "moveoutDate": case.moveout_date 
+                    })
+        return Response(data)
 
 class WelcomePageView(TemplateView):
     template_name = 'landlord_finder/welcome.html'
