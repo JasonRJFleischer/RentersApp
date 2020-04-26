3514a17f4294de0efc415aefa369af69b7d9a598
Lance Godard <45862269+LGG233@users.noreply.github.com>
2020-04-26 18:32:15 -0400

    Added Search Capability (#10)

---
+++ client/src/components/index.js
@@ -0,0 +1,9 @@
+import evictionDisplay from './evictionDisplay';
+import NavBar from './navbar.jsx';
+import Home from './home';
+import Photos from './photoLibrary';
+import Resources from './resourceRoom';
+import Landlords from './landlords';
+import SearchDB from './searchDB';
+
+export { SearchDB, evictionDisplay, NavBar, Home, Photos, Landlords, Resources, }
