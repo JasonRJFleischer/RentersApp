4b56587130c4f4102becc411fafc7f36a88557fb
Fred Astaire <lancebc@thegodardgroup.com>
2020-04-28 01:08:28 -0400

    edited searchDB to display results

--- client/src/components/searchDB.jsx
+++ client/src/components/searchDB.jsx
@@ -11,6 +11,7 @@ class SearchDB extends Component {
       zipCode: "",
       filingDate: "",
       moveoutDate: "",
+      data: [],
     };
   }
 
@@ -57,80 +58,117 @@ class SearchDB extends Component {
   };
 
   render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <h4>Search</h4>
-          <div className="container">
-            <div className="FormCenter">
-              <form onSubmit={this.handleSubmit} className="FormField">
-                <div className="container">
-                  <div className="FormField">
-                    <label for="plaintiff">Landlord</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="plaintiff"
-                      name="plaintiff"
-                      value={this.state.plaintiff}
-                      onChange={this.handleChange}
-                    />
+    if (this.state.data.length === 0) {
+      return (
+        <div className="container-fluid">
+          <div className="row">
+            <h4>Search</h4>
+            <div className="container">
+              <div className="FormCenter">
+                <form onSubmit={this.handleSubmit} className="FormField">
+                  <div className="container">
+                    <div className="FormField">
+                      <label for="plaintiff">Landlord</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="plaintiff"
+                        name="plaintiff"
+                        value={this.state.plaintiff}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="FormField">
+                      <label for="houseNumber">House Number</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="houseNumber"
+                        name="houseNumber"
+                        value={this.state.houseNumber}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="FormField">
+                      <label for="streetName">
+                        Street Name (do not include "Street," "Blvd," etc
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="streetName"
+                        name="streetName"
+                        value={this.state.streetName}
+                        onChange={this.handleChange}
+                      />
+                    </div>
+                    <div className="FormField">
+                      <label for="zipCode">Zip Code</label>
+                      <input
+                        type="number"
+                        className="form-control"
+                        id="zipCode"
+                        name="zipCode"
+                        value={this.state.zipCode}
+                        onChange={this.handleChange}
+                      />
+                    </div>
                   </div>
-                  <div className="FormField">
-                    <label for="houseNumber">House Number</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="houseNumber"
-                      name="houseNumber"
-                      value={this.state.houseNumber}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="FormField">
-                    <label for="streetName">
-                      Street Name (do not include "Street," "Blvd," etc
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="streetName"
-                      name="streetName"
-                      value={this.state.streetName}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="FormField">
-                    <label for="zipCode">Zip Code</label>
-                    <input
-                      type="number"
-                      className="form-control"
-                      id="zipCode"
-                      name="zipCode"
-                      value={this.state.zipCode}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                </div>
-                <br></br>
-                <button
-                  className="btn btn-sm btn-primary searchSubmit"
-                  onClick={this.handleSubmit}
-                >
-                  Lookup{" "}
-                </button>
-                <button
-                  className="btn btn-sm btn-primary searchCancel"
-                  onClick={this.handleCancel}
-                >
-                  Cancel{" "}
-                </button>
-              </form>
+                  <br></br>
+                  <button
+                    className="btn btn-sm btn-primary searchSubmit"
+                    onClick={this.handleSubmit}
+                  >
+                    Lookup{" "}
+                  </button>
+                  <button
+                    className="btn btn-sm btn-primary searchCancel"
+                    onClick={this.handleCancel}
+                  >
+                    Cancel{" "}
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <h4>Search Results</h4>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Plaintiff</th>
+                <th scope="col">Address</th>
+                <th scope="col">Filing Date</th>
+                <th scope="col">Move Out Date</th>
+                {/* <th scope="col">Tax Info</th> */}
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((data) => (
+                <tr>
+                  <th>{data.plaintiff}</th>
+                  <td scope="row">
+                    {data.address.addressLine1}
+                    <br></br>
+                    {/* {data.address.addressLine2}
+                  <br></br>
+                  {data.address.addressLine3}
+                  <br></br> */}
+                    {data.address.city}, {data.address.state} {data.address.zip}
+                  </td>
+                  <td>{data.filingDate}</td>
+                  <td>{data.moveoutDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 export default SearchDB;
